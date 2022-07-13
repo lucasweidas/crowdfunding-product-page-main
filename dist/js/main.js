0431a938e951a2f689ad72b905797034864926f6
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2022-07-13 18:38:15 -0300

    mobile navigation features finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,36 @@
+const header = document.querySelector('[data-header]');
+const toggleMenu = document.querySelector('[data-toggle-menu]');
 
+header.addEventListener('click', checkHeaderClick);
+
+function checkHeaderClick(evt) {
+  const target = evt.target;
+
+  if (target.matches('[data-toggle-menu]')) {
+    return mobileMenu(target);
+  }
+}
+
+function mobileMenu(button) {
+  const menu = document.querySelector('[data-nav-menu]');
+
+  toggleActiveClass([menu, button]);
+  const isActive = menu.classList.contains('active');
+  button.setAttribute('aria-expanded', isActive);
+  button.setAttribute('aria-pressed', isActive);
+  if (!isActive) {
+    menu.classList.add('closing');
+    menu.addEventListener(
+      'animationend',
+      () => {
+        menu.classList.remove('closing');
+      },
+      { once: true }
+    );
+  }
+}
+
+function toggleActiveClass(elements) {
+  elements.forEach(element => element.classList.toggle('active'));
+}
 //# sourceMappingURL=main.js.map
