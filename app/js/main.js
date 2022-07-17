df517e9d89b130582ed6d3b83e4a3fb0aad6319d
Lucas Weidas <90000226+lucasweidas@users.noreply.github.com>
2022-07-16 21:42:52 -0300

    project update

--- app/js/main.js
+++ app/js/main.js
@@ -2,27 +2,29 @@ const header = document.querySelector('[data-header]');
 const main = document.querySelector('[data-main]');
 const selectionModal = document.querySelector('[data-modal-selection]');
 const form = document.querySelector('[data-form]');
+const rewardItem = {
+  selectedItem: null,
+  previousItem: null,
+  isActive: false,
+  wrapperGrow: null,
+  growChild: null,
+  childHeight: 0,
+};
 
 header.addEventListener('click', checkHeaderClick);
 main.addEventListener('click', checkMainClick);
+main.addEventListener('change', checkMainChange);
 form.addEventListener('submit', evt => {
   evt.preventDefault();
 });
 
-function checkHeaderClick(evt) {
-  const target = evt.target;
-
+function checkHeaderClick({ target }) {
   if (target.hasAttribute('data-toggle-menu') || target.hasAttribute('data-overlay')) {
     return toggleMobileMenu();
   }
-  // if (target.hasAttribute('data-overlay')) {
-  //   return toggleMobileMenu();
-  // }
 }
 
-function checkMainClick(evt) {
-  const target = evt.target;
-
+function checkMainClick({ target }) {
   if (target.hasAttribute('data-button-bookmark')) {
     return toggleBookmark();
   }
@@ -37,6 +39,15 @@ function checkMainClick(evt) {
   }
 }
 
+function checkMainChange(evt) {
+  const { target } = evt;
+  if (target.hasAttribute('data-reward-radio')) {
+    rewardRadioSelected(target);
+    console.log(target.checked);
+  }
+}
+
+// Mobile Navigation Menu
 function toggleMobileMenu() {
   const nav = document.querySelector('[data-nav]');
   const button = document.querySelector('[data-toggle-menu]');
@@ -51,15 +62,7 @@ function toggleMobileMenu() {
   nav.addEventListener('animationend', toggleClosingClass);
 }
 
-function toggleActiveClass(element) {
-  element.classList.toggle('active');
-}
-
-function toggleClosingClass() {
-  this.classList.toggle('closing');
-  this.removeEventListener('animationend', toggleClosingClass);
-}
-
+// Bookmark Button
 function toggleBookmark() {
   const isBookmarked = !JSON.parse(localStorage.getItem('bookmark'));
   const html = document.documentElement;
@@ -82,12 +85,54 @@ function setToggleBookmarkAttributes() {
   }
 }
 
+// Selection Modal
 function openSelectionModal() {
   toggleActiveClass(selectionModal);
+  window.addEventListener('resize', setElementHeight);
 }
 
 function closeSelectionModal() {
   toggleActiveClass(selectionModal);
   selectionModal.classList.add('closing');
   selectionModal.addEventListener('animationend', toggleClosingClass);
+}
+
+function rewardRadioSelected(radioButton) {
+  rewardItem.selectedItem = getParent(radioButton, 'data-reward-item');
+  rewardItem.previousItem = rewardItem.selectedItem;
+  rewardItem.isActive = true;
+  rewardItem.wrapperGrow = rewardItem.selectedItem.querySelector('[data-wrapper-grow]');
+  rewardItem.growChild = rewardItem.wrapperGrow.firstElementChild;
+  console.log(rewardItem.growChild);
+
+  toggleActiveClass(rewardItem.selectedItem);
+  setElementHeight();
+  console.log(rewardItem.wrapperGrow);
+}
+
+// Helpers
+function toggleActiveClass(element) {
+  element.classList.toggle('active');
+}
+
+function toggleClosingClass() {
+  this.classList.toggle('closing');
+  this.removeEventListener('animationend', toggleClosingClass);
+}
+
+function getParent(element, attribute) {
+  const parent = element.parentElement;
+  if (parent.hasAttribute(attribute)) return parent;
+  return getParent(parent, attribute);
+}
+
+function setElementHeight() {
+  const height = rewardItem.growChild.offsetHeight;
+  rewardItem.childHeight = height;
+
+  if (rewardItem.isActive) {
+    rewardItem.wrapperGrow.style.maxHeight = `${height}px`;
+  } else {
+    rewardItem.wrapperGrow.style.maxHeight = '0px';
+  }
 }
